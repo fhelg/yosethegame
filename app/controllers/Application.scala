c79020a1ee64740b863c89af19424b04b41260de
François Helg <francois@theagilist.ch>
2014-02-27 13:45:06 +0100

    Answer ping question

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,16 @@ package controllers
 
 import play.api.mvc.{Action, Controller}
 import play.api.http.MimeTypes
+import play.api.libs.json.Json
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.Index())
+  }
+
+  def ping = Action {
+    Ok(Json.obj("alive" -> true))
   }
   
 }
